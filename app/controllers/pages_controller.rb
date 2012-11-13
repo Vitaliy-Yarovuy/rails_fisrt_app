a2e926ff4dad6005f7e153c98125730687f97138
vitaliy <vitaliy.yxz@gmail.com>
2012-11-13 23:10:04 +0200

    add micropost

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
 	def home
+		@title = "Home"
+		if signed_in?
+			@micropost = Micropost.new
+			@feeds_items = current_user.feed.paginate(:page => params[:page])
+		end
+
 	end
 
 	def about
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,10 @@ class User < ActiveRecord::Base
 		encrypted_password == encrypt(submitted_password)
 	end
 
+	def feed
+		micropost
+	end
+
 	def self.authenticate(email,submitted_password)
 		user = find_by_email(email)
 		return nil if user.nil?
